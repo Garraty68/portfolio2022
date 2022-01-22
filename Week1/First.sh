35fd94b2e3ad438a325d6992b824dee9d0615861
Mike Sinfield <mjsinfie@our.ecu.edu.au>
2022-01-22 10:11:57 +0000

    Initial commit

---
+++ Week1/First.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello!"
+echo "My name is Bruce!"
+echo "Talk about a steep learning curve!"
+exit 0
---
+++ Week1/euex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apiPath = "https://api.exchangerate-api.com/v4/latest/EUR"
+fileName="index.html"
+Browsers= "google-chrome firefox"
+if $Browsers
+then
+google-chrome $fileName&
+else
+firefox $fileName&
+fi
+while :;
+do
+#<--code-->
+done
